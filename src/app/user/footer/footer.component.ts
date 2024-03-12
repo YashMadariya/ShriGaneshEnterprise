4d0fba21202da1bdc6bda7ea07bfa5b6474bc5af
yash <devext04@technolink.it>
2024-03-12 23:25:57 +0530

    build files

--- src/app/user/footer/footer.component.ts
+++ src/app/user/footer/footer.component.ts
@@ -13,16 +13,4 @@ export class FooterComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  downloadFile(): void {
-    const fileUrl = 'https://drive.google.com/file/d/1M86V8dZD5DJZa6Fse_enEwfYJc4fwotZ/view?usp=drive_link'; // Replace with the actual file URL
-    const filename = 'SHRI GANESH ENTRPRISE 2024.pdf'; // Replace with the desired filename
-
-    this.fileDownloadService.downloadFile(fileUrl, filename).subscribe(blob => {
-      const link = document.createElement('a');
-      link.href = window.URL.createObjectURL(blob);
-      link.download = filename;
-      link.click();
-    });
-  }
-
 }
